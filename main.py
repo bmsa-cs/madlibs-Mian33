4ea19eb17c7b0d9efb4100450d2630a1af513634
Ian McFedries <imcfedries24@biomedscienceacademy.org>
2020-11-09 19:25:06 +0000

    I finished my MadLib and turned it in.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Ian McFedries
+Period/Core: Core 3
 
 
 """
+
+adjective1 = input("Adjective: ")
+adjective2 = input("Adjective: ")
+adjective3 = input("Adjective: ")
+name = input("Your first name: ")
+vehical =  input("flying vehicle: ")
+summer_game= input("fun game: ")
+play = input("favorite play: ")
+actor = input("favorite actor: ")
+activity = input("Fun activity:")
+place = input("Place:")
+
+print(f"\n\nCongratulations {name},\nYou have you have won a {adjective1} trip to hawaii.\nIf you accept you will be flown out on a {adjective2} {vehical} and do \nthe following activities:\n   1. You will be playing {summer_game}\n   2. Then you will go to a {adjective3} play of {play}\n   3. After, you will play {activity} with {actor}\nTo sum it all up you will have a spaw at {place}.\nCan't wait to see you there!")
